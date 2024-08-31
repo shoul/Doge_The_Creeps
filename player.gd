538115977956da0420ecaf947ca6c8e0d9d2f3ce
Frank Schneider <shoul@gmx.de>
2024-08-31 20:02:39 +0200

    Add body to body collision. Player hiden at start.

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends Area2D
 @export var speed = 400
 var screen_size
+signal hit
 
 
 # Called when the node enters the scene tree for the first time.
@@ -38,3 +39,17 @@ func _process(delta: float) -> void:
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties 
+	# on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+		
