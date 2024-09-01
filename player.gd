11b199300f676e3f78d5b8b07d55d6a853631a7b
Frank Schneider <shoul@gmx.de>
2024-09-01 09:24:20 +0200

    Mob scene and main scene added. Code not working.

--- player.gd
+++ player.gd
@@ -41,7 +41,7 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	hide() # Player disappears after being hit.
 	hit.emit()
 	# Must be deferred as we can't change physics properties 
